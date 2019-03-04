f72ea0f1ab78ef34e959394eae61aba98d1a8594
519628wenlin <wenlin>
2019-03-04 17:46:04 +0800

    返回json格式数据

--- demo-user-consumer/src/main/java/wl/controller/UserController.java
+++ demo-user-consumer/src/main/java/wl/controller/UserController.java
@@ -1,13 +1,13 @@
 package wl.controller;
 
 import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.wl.entity.req.UserEntityReq;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.wl.entity.resp.UserEntityResp;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import wl.common.ResBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,21 +17,24 @@ import javax.ws.rs.core.Context;
 @Component
 @RequestMapping(value="/demo/user")
 public class UserController {
-    @Consumes()
+    @Consumes(ContentType.APPLICATION_JSON_UTF_8)
     @Produces(ContentType.APPLICATION_JSON_UTF_8)
-    @RequestMapping(value="/getUserInfo", method= RequestMethod.POST)
+    @RequestMapping(value="/getUserInfo", method= {RequestMethod.POST,RequestMethod.GET})
+    @ResponseBody
     public ResBody<UserEntityResp> getUserInfo(@Context HttpServletRequest request, String data){
-         UserEntityReq userEntityReq = JSONObject.parseObject(data, UserEntityReq.class);
+        // UserEntityReq userEntityReq = JSONObject.parseObject(data, UserEntityReq.class);
          ResBody<UserEntityResp> resBody = new  ResBody<UserEntityResp>();
-         System.out.println(JSON.toJSONString(userEntityReq));
+         //System.out.println(JSON.toJSONString(userEntityReq));
         UserEntityResp ss=new UserEntityResp();
         ss.setAge(11);
         ss.setSex("nan");
         ss.setUserId("1");
+        ss.setUserName("nihj");
         resBody.setRetCode("00000");
         resBody.setRetDesc("成功");
         resBody.setRspBody(ss);
-         return resBody;
+        System.out.println(JSONObject.toJSONString(resBody));
+        return resBody;
 
     }
 
